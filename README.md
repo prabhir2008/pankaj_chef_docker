31f71106d3d800725bf6c71e47d2910b7571f3af
prabhir <pankaj.c.pimple@gmail.com>
2017-09-03 21:12:23 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# pankaj_chef_docker
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'pankaj_chef_docker'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures pankaj_chef_docker'
+long_description 'Installs/Configures pankaj_chef_docker'
+version '0.1.0'
